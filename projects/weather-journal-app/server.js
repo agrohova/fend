f51bbf71fa13f738d271bde20b186eb430df827b
Anna Grohová <anna.grohova@gmx.at>
2023-06-23 23:40:23 +0200

    Establish Server-side and Client side code
    - establish server
    - establish get and post routes for server and client

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,51 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//Here we are configuring express to use body-parser as middle-ware. *as body parser has been deprecated, using
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+// Establish GET route
+app.get('/data', sendData);
+
+function sendData (req, res) {
+  res.send(projectData);
+};
+
+// Establish POST route
+const data = []
+
+app.post('/addData', addData);
+
+function addData (req, res){
+    const {temperature, date, response} = req.body;
+    const newData = {temperature, date, response}
+    console.log(newData);
+    data.push(newData);
+// help code - info message
+res.status(200).json({ message: 'Data added successfully' });
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -3,3 +3,24 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Establish POST route
+const postData = async (url = '', data = {}) =>{
+    console.log(data);
+        const res = awaiat fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch(error) {
+    console.log("error", error)
+    }
+}
