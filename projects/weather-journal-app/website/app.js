62ba11b50b95087e7a130a9f287d5dfdd5049f07
Anna Grohová <anna.grohova@gmx.at>
2023-06-23 23:44:02 +0200

    Client code POST route
    - correcting a typo in 'await'

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -7,7 +7,7 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 // Establish POST route
 const postData = async (url = '', data = {}) =>{
     console.log(data);
-        const res = awaiat fetch(url, {
+        const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
