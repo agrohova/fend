4b86306cdd08820bbbc7a1852b6b0d361a75a121
Anna Grohová <anna.grohova@gmx.at>
2023-07-16 22:29:04 +0200

    Removing style-loader from webpack.prod
    - apparently there's a conflict between minicssextractplugin.loader and styleloader

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -16,7 +16,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
