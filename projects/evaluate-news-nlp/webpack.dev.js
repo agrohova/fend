2caf8efd4fe22d4b27938eefe1dbf0b251fd3362
Anna Grohová <anna.grohova@gmx.at>
2023-07-16 13:43:40 +0200

    Remove MiniCssExtractPlugin.loader
    - because it caused errors on the build

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,7 +17,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ['style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -35,7 +34,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),
-        new MiniCssExtractPlugin({ filename: "[name].css" })
+        })
     ]
 }
