4c81e8d84c1055c4805602a24f0c2a5d51d0601d
Anna Grohová <anna.grohova@gmx.at>
2023-07-16 17:25:22 +0200

    Import URL checker to HTML
    - import URL checker, delete nameChecker (from the lesson's material)

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,12 +1,7 @@
-import { checkForName } from './js/nameChecker'
+import { checkURL } from './js/checkURL'
 import { handleSubmit } from './js/formHandler'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
